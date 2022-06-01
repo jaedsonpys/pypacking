a7d1acda45fdc2c8cf0a9b2500b6ae979e860a55
Jaedson Silva <contacanal798@gmail.com>
2022-05-31 23:13:44 -0300

    adding test_make_config unit testing

--- tests/pypacking_test.py
+++ tests/pypacking_test.py
@@ -11,6 +11,16 @@ class PyPackingTest(SeqTest):
     def __init__(self):
         super().__init__()
 
+    def test_make_config(self):
+        pypacking = PyPacking.make_config(
+            project_name='PyPacking',
+            description='Simple description',
+            version='1.0.0',
+            package_path='pypacking'
+        )
+
+        self.is_true(os.path.isfile('./pypacking.ini'), msg_error='Config file not created')
+
 
 if __name__ == '__main__':
     PyPackingTest().run()
