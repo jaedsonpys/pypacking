342c6696954c0d2d41e3b6b987457beb0db90170
Jaedson Silva <contacanal798@gmail.com>
2022-06-01 15:04:24 -0300

    adding argument to generate config file for project

--- pypacking/main.py
+++ pypacking/main.py
@@ -9,5 +9,23 @@ def main():
         version='1.0.0'
     )
 
+    parser.add_argument('generate_config', 'Generate a default config file', action='store_true')
+    args = parser.get_args()
+
+    if args.generate_config:
+        project_name = input('Name: ').strip()
+        project_description = input('Description (default description): ').strip()
+        project_version = input('Version (1.0.0): ').strip()
+        package_path = input('Python package: ').strip()
+
+        if not project_version:
+            project_version = '1.0.0'
+        if not project_description:
+            project_description = f'The {project_name} project'
+
+        PyPacking.make_config(project_name, project_description, project_version, package_path)
+        print('-' * 50)
+        print('Project config \033[1mcreated\033[m! Check "pypacking.ini" file.')
+
 
 main()
