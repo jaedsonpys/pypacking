f3da230c31e538c3ab4de3f44f2e0e14f3601d0b
Jaedson Silva <contacanal798@gmail.com>
2022-06-01 15:01:24 -0300

    creating main.py file and adding ArgEasy argument parser

---
+++ pypacking/main.py
@@ -0,0 +1,13 @@
+from argeasy import ArgEasy
+from pypacking import PyPacking
+
+
+def main():
+    parser = ArgEasy(
+        project_name='PyPacking',
+        description='Build project packages',
+        version='1.0.0'
+    )
+
+
+main()
