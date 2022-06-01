9578f457df06dfd3f1552b1b660d0c107b36a122
Jaedson Silva <contacanal798@gmail.com>
2022-05-31 22:36:17 -0300

    creating PyPacking class and getting project settings in init() method

---
+++ pypacking/pypacking.py
@@ -0,0 +1,14 @@
+from configparser import ConfigParser
+
+
+class PyPacking:
+    def __init__(self):
+        project_config = ConfigParser()
+        project_info = project_config['INFO']
+        package_info = project_config['PACKAGE']
+
+        self.project_name = project_info['projectName']
+        self.project_version = project_info['version']
+        self.project_description = project_info['description']
+
+        self.package_path = package_info['packagePath']
