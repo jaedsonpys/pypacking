e89362eaaa924ce01527085080fe1380364e4784
Jaedson Silva <contacanal798@gmail.com>
2022-06-06 13:31:51 -0300

    import InvalidConfigError exception in __init__

--- pypacking/__init__.py
+++ pypacking/__init__.py
@@ -1,6 +1,7 @@
 from .pypacking import PyPacking
 from .exceptions import PackageNotFoundError
 from .exceptions import ConfigFileNotFoundError
+from .exceptions import InvalidConfigFileError
 
 __author__ = 'Jaedson Silva'
 __version__ = '2.0.0'
