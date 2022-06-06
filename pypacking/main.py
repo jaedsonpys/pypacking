b277f59b46192d253231c28fecfc6024edb7944f
Jaedson Silva <contacanal798@gmail.com>
2022-06-06 13:35:32 -0300

    changing argeasy project description

--- pypacking/main.py
+++ pypacking/main.py
@@ -10,7 +10,10 @@ from .pypacking import PyPacking
 def main():
     parser = ArgEasy(
         project_name='PyPacking',
-        description='Build project packages',
+        description='PyPacking is a package manager and' \
+                    'installer for Python designed to be' \
+                    'interactive and user-friendly.\nWith' \
+                    'easy operation, you can install packages and tools.',
         version='2.0.0'
     )
 
