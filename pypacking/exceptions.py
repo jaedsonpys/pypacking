7ee382250811be7f8fba213051d70e106b909686
Jaedson Silva <contacanal798@gmail.com>
2022-06-06 13:06:11 -0300

    adding InvalidConfigFileError exception

--- pypacking/exceptions.py
+++ pypacking/exceptions.py
@@ -6,3 +6,8 @@ class ConfigFileNotFoundError(Exception):
 class PackageNotFoundError(Exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
+
+
+class InvalidConfigFileError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
