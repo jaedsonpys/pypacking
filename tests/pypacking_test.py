9b5b0213db07efb7729bdf3ae6ce471e6a14260e
Jaedson Silva <contacanal798@gmail.com>
2022-06-07 15:52:30 -0300

    updating test_make_config arguments

--- tests/pypacking_test.py
+++ tests/pypacking_test.py
@@ -13,10 +13,13 @@ class PyPackingTest(SeqTest):
 
     def test_make_config(self):
         pypacking = PyPacking.make_config(
+            author_name='Jaedson Silva',
+            author_email='imunknowuser@protonmail.com',
             project_name='PyPacking',
             description='Simple description',
             version='1.0.0',
-            package_path='pypacking'
+            package_path='pypacking',
+            script_entry='pypacking:main:main'
         )
 
         self.is_true(os.path.isfile('./pypacking.ini'), msg_error='Config file not created')
