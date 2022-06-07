f29bd5069856ae8c2173ecaf0de9d2ce89b26f50
Jaedson Silva <contacanal798@gmail.com>
2022-06-07 15:58:02 -0300

    testing PyPacking.read_config method

--- tests/pypacking_test.py
+++ tests/pypacking_test.py
@@ -24,6 +24,17 @@ class PyPackingTest(SeqTest):
 
         self.is_true(os.path.isfile('./pypacking.ini'), msg_error='Config file not created')
 
+    def test_read_config(self):
+        pypacking = PyPacking()
+        pypacking.read_config()
+
+        self.is_true(pypacking.author_name == 'Jaedson Silva', msg_error='Author name wrong')
+        self.is_true(pypacking.author_email == 'imunknowuser@protonmail.com', msg_error='Author email wrong')
+        self.is_true(pypacking.project_name == 'PyPacking', msg_error='Project name wrong')
+        self.is_true(pypacking.project_description == 'Simple description', msg_error='Description wrong')
+        self.is_true(pypacking.project_version == '1.0.0', msg_error='Version wrong')
+        self.is_true(pypacking.package_path == 'pypacking', msg_error='Package path wrong')
+
 
 if __name__ == '__main__':
     PyPackingTest().run()
