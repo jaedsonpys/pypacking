f5060b0989187d186421316dbcf3658ffd5c0a74
Jaedson Silva <contacanal798@gmail.com>
2022-06-07 16:09:31 -0300

    testing PyPacking.make_package method

--- tests/pypacking_test.py
+++ tests/pypacking_test.py
@@ -36,5 +36,29 @@ class PyPackingTest(SeqTest):
         self.is_true(pypacking.package_path == 'pypacking', msg_error='Package path wrong')
 
 
+    def test_make_package(self):
+        pypacking = PyPacking()
+        
+        try:
+            pypacking.read_config()
+        except ConfigFileNotFoundError as error:
+            self.is_true(False, msg_error=error.msg)
+        except PackageNotFoundError as error:
+            self.is_true(False, msg_error=error.msg)
+        except InvalidConfigFileError as error:
+            self.is_true(False, msg_error=error.msg)
+
+        pypacking.make_package()
+
+        self.is_true(os.path.isdir('build'), msg_error='build/ directory not created')
+
+        # checking build directory
+        self.is_true(os.path.isdir('build/pypacking'), msg_error='build/pypacking directory not created')
+        self.is_true(os.path.isfile('build/pypacking.ini'), msg_error='build/pypacking.ini file not created')
+
+        self.is_true(os.path.isdir('dist'), msg_error='dist/ directory not created')
+        self.is_true(os.path.isfile('dist/PyPacking-1.0.0.zip'), msg_error='Package file not created')
+
+
 if __name__ == '__main__':
     PyPackingTest().run()
